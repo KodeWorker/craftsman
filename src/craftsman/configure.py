655f9171abb9725f6e2ac98d71b40558dcee13e7
Kelvin_Wu <Kelvin_Wu@asus.com>
2026-04-21 18:55:04 +0800

    windows support

--- src/craftsman/configure.py
+++ src/craftsman/configure.py
@@ -8,8 +8,8 @@ _USER_CONFIG = os.path.expanduser("~/.craftsman/craftsman.yaml")
 
 def get_config():
     if os.path.exists(_USER_CONFIG):
-        with open(_USER_CONFIG) as f:
+        with open(_USER_CONFIG, encoding="utf-8") as f:
             return yaml.safe_load(f)
     config_path = files("craftsman").joinpath("craftsman.yaml")
-    with config_path.open() as f:
+    with config_path.open(encoding="utf-8") as f:
         return yaml.safe_load(f)
